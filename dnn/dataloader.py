79957ff08bcefd1a1893520f187e6934f373900c
Cheny <yongqi.chen@skysys.cn>
2024-09-24 11:13:37 +0800

    修复dataset 加载数据的逻辑

--- dnn/dataloader.py
+++ dnn/dataloader.py
@@ -1,5 +1,6 @@
 
 
+from audioop import bias
 import os
 import numpy as np
 from rich.console import Console
@@ -7,17 +8,18 @@ from torch.utils.data.dataloader import _collate_fn_t, _worker_init_fn_t
 from torch.utils.data.dataset import Dataset
 
 class AirsimDataset(Dataset):
-    def __init__(self,data_file,logger:Console=None) -> None:
+    def __init__(self,data_file,bias=40,logger:Console=None) -> None:
         super(AirsimDataset,self).__init__()
         self.logger=logger
+        self.bias =bias
         if not os.path.exists(data_file):
             self.logger.log(f"Can't load dataset from file {data_file},file not exists!!!",style="red")
         self.data=np.load(data_file,allow_pickle=True)
     
     def __getitem__(self, index):
-        data = self.data[index]
-        return data[:]
-    
+        data = self.data[index:self.bias+index]
+        gt  = self.data[self.bias+index][:12]
+        return data,gt    
     def __len__(self):
-        return len(self.data)
+        return len(self.data)-bias
     
